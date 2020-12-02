b4ab96b346cbb848308d1b84cbbf8080c371a2d4
OriginalOrgangeXD <349604207@gapps.yrdsb.ca>
2020-12-02 10:56:02 -0500

    day02

---
+++ day02/CMakeFiles/day01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day01.dir/src/main.cpp.o"
+  "day01"
+  "day01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/day01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ day02/CMakeFiles/day02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day02.dir/src/main.cpp.o"
+  "day02"
+  "day02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/day02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
